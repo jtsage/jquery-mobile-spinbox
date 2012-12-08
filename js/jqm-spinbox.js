edd77c5497f1eea84ab01034f76c7134efc3e656
J.T. Sage <jtsage+github@gmail.com>
2012-12-07 20:11:07 -0500

    Add "vertical" mode, clean more css up

--- js/jqm-spinbox.js
+++ js/jqm-spinbox.js
@@ -13,7 +13,8 @@
 			dmax: false,
 			theme: false,
 			initSelector: "input[data-role='spinbox']",
-			clickEvent: 'vclick'
+			clickEvent: 'vclick',
+			type: 'horizontal', // or vertical
 		},
 		_create: function() {
 			var w = this, tmp,
@@ -30,29 +31,40 @@
 			}
 			
 			w.d.wrap
-				.css({'display':'inline','whiteSpace':'nowrap','border':'none'})
 				.removeClass('ui-input-text ui-shadow-inset ui-btn-shadow')
-				.addClass('ui-controlgroup ui-controlgroup-horizontal ui-controlgroup-controls');
+				.addClass('ui-controlgroup ui-controlgroup-'+o.type);
+				
+			if ( o.type === "horizontal" ) { 
+				w.d.wrap.css({'display':'inline', 'whiteSpace':'nowrap', 'border':'none'}); 
+				w.d.input.addClass('ui-slider-input');
+			} else {
+				w.d.input.css({'width':'auto'});
+				w.d.wrap.css({'width':'auto','display':'inline-block'});
+			}
+			w.d.input.css({'textAlign':'center'});
 			
 			if ( o.theme === false ) {
 				o.theme = $(this).closest('[data-theme]').attr('data-theme');
 				if ( typeof o.theme === 'undefined' ) { o.theme = 'c'; }
 			}
 			
-			w.d.input.removeClass('ui-input-text ui-corner-all').addClass('ui-slider-input');
 			
 			if ( o.dmin === false ) { o.dmin = ( typeof w.d.input.attr('min') !== 'undefined' ) ? parseInt(w.d.input.attr('min'),10) : Number.MAX_VALUE * -1; }
 			if ( o.dmax === false ) { o.dmax = ( typeof w.d.input.attr('max') !== 'undefined' ) ? parseInt(w.d.input.attr('max'),10) : Number.MAX_VALUE; }
 			
 			w.d.up = $('<div>')
-				.buttonMarkup({icon: 'plus', theme: o.theme, iconpos: 'notext', corners:true, shadow:true, inline:true})
-				.css({'marginLeft':'0px', 'marginBottom':'0px', 'marginTop':'0px', 'paddingLeft':'.4em'})
-				.appendTo(w.d.wrap);
+				.buttonMarkup({icon: 'plus', theme: o.theme, iconpos: 'notext', corners:true, shadow:true, inline:o.type==="horizontal"})
+				.css({'marginLeft':'0px', 'marginBottom':'0px', 'marginTop':'0px', 'paddingLeft':o.type==="horizontal"});
 				
 			w.d.down = $('<div>')
-				.buttonMarkup({icon: 'minus', theme: o.theme, iconpos: 'notext', corners:true, shadow:true, inline:true})
-				.css({'marginRight':'0px', 'marginBottom':'0px', 'marginTop':'0px', 'paddingRight':'.4em'})
-				.prependTo(w.d.wrap);
+				.buttonMarkup({icon: 'minus', theme: o.theme, iconpos: 'notext', corners:true, shadow:true, inline:o.type==="horizontal"})
+				.css({'marginRight':'0px', 'marginBottom':'0px', 'marginTop':'0px', 'paddingRight':o.type==="horizontal"?'.4em':'0px'});
+				
+			if ( o.type === 'horizontal' ) {
+				w.d.down.prependTo(w.d.wrap); w.d.up.appendTo(w.d.wrap);
+			} else {
+				w.d.up.prependTo(w.d.wrap); w.d.down.appendTo(w.d.wrap);
+			}
 				
 			$.mobile.behaviors.addFirstLastClasses._addFirstLastClasses(w.d.wrap.find('.ui-btn'), w.d.wrap.find('.ui-btn'), true);
 			
